fe3bbe37cc474a5bf463f55becabfc0ab6e8f4a8
arterII <arne.dummer@googlemail.com>
2020-09-17 08:09:04 +0200

    Create midi file service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MidiFileService } from './service/midi-file.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers:    [ MidiFileService ]
 })
 export class AppModule { }
