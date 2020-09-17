15a2eb579fe9855188370d563f3530ff58d9e9c9
Arne Dummer <arne.dummer@hmmh.de>
2020-09-17 10:24:27 +0200

    make octets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import {Component, OnInit, VERSION} from '@angular/core';
+import {MidiFileService} from "./service/midi-file.service";
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+
+  constructor(private midiFileService: MidiFileService) {
+  }
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit(): void {
+    console.log(this.midiFileService.intToOctets(844,8,3));
+    console.log(this.midiFileService.intToOctets(63433,8,3));
+    console.log(this.midiFileService.intToOctets(1263433,8,1));
+  }
 }
--- src/app/service/midi-file.service.ts
+++ src/app/service/midi-file.service.ts
@@ -3,4 +3,15 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class MidiFileService {
 
+    intToOctets(intValue: number, radix: number, length): number[] {
+        const mask = 2**radix-1;
+        const octets: number[] = [];
+        octets.length = length;
+        octets.fill(0);
+        let indx= 0;
+        for(let i = intValue; i != 0; i>>>=radix) {
+            octets[indx++] = i & mask;
+        }
+        return octets.reverse();
+    }
 }
